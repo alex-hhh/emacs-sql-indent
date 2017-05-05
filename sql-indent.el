59725cbb33e3eff5e378bb2483157b772af6905c
Pierre Téchoueyres <pierre.techoueyres@free.fr>
2017-05-06 00:16:31 +0200

    Fix indentation.
    Previous version didn't work.

--- sql-indent.el
+++ sql-indent.el
@@ -566,9 +566,12 @@ See also `sqlind-beginning-of-block'"
 	    (goto-char (match-end 0))
             (sqlind-forward-syntactic-ws)
 	    ;; skip param list, if any.
-	    (when (looking-at "(") (ignore-errors (forward-sexp 1)))
-	    (when (looking-at "return\\(?:[ \n\r\f]+\\)\\([a-z0-9_]+\\)") (goto-char (match-end 0)))
-	    (sqlind-forward-syntactic-ws)
+	    (when (looking-at "(")
+	      (ignore-errors (forward-sexp 1))
+	      (sqlind-forward-syntactic-ws))
+	    (when (looking-at "return\\(?:[ \n\r\f]+\\)\\([a-z0-9_]+\\)")
+	      (goto-char (match-end 0))
+	      (sqlind-forward-syntactic-ws))
 	    (when (looking-at ";")
 	      ;; not a procedure after all.
 	      (throw 'exit nil)))
